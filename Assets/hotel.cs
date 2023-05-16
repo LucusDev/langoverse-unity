4b8b292a6acd3821002496f9d224354e20ce2b44
Htetaungshine11 <newbtcearner@gmail.com>
2023-05-17 00:16:15 +0630

    zoom and touch added

--- Assets/hotel.cs
+++ Assets/hotel.cs
@@ -1,34 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using FlutterUnityIntegration;
 
 public class hotel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public new Camera camera;
+
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //RaycastHit hit = new RaycastHit();
-        //for (int i = 0; i < Input.touchCount; ++i)
-        //{
-        //    if (Input.GetTouch(i).phase.Equals(TouchPhase.Began))
-        //    {
-        //        // Construct a ray from the current touch coordinates
-        //        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-        //        if (Physics.Raycast(ray, out hit))
-        //        {
 
-        //        }
-        //    }
-        //}
-        UnityMessageManager.Instance.SendMessageToFlutter("your-message-here");
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
 
+            Ray ray = camera.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100f);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
 
+                    GameObject touchedObject = hit.transform.gameObject;
+                    UnityMessageManager.Instance.SendMessageToFlutter(touchedObject.transform.name);
+
+                    Debug.Log("Touched " + touchedObject.transform.name);
+                }
+            }
+        }
     }
+   
+
 }
+
